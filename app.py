701a000b83150aaa80bf3c6c69fff6945c815270
kefrankk <ke.heckler95@gmail.com>
2024-09-17 10:45:20 -0300

    feat: add data validation

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
 
 import streamlit as st
+from contract import Sales
+from datetime import datetime, time
+from pydantic import ValidationError
 
 def main():
 
     st.title("CRM System and Sales")
 
     email       = st.text_input("Seller's Email")
-    date        = st.date_input("Date of Sale")
-    time        = st.time_input("Time of Sale")
-    value       = st.number_input("Sale Value")
-    n_produts   = st.number_input("Number of Products Sold", min_value=0)
-    catogory    = st.selectbox("Category of Product Sold", ["Category 1", "Category 2", "Category 3"])
+    s_date      = st.date_input("Date of Sale", datetime.now())
+    s_time      = st.time_input("Time of Sale", time(9, 0))
+    value       = st.number_input("Sale Value", min_value=0.0)
+    n_products   = st.number_input("Number of Products Sold", min_value=1, step=1)
+    category    = st.selectbox("Category of Product Sold", ["Category 1", "Category 2", "Category 3"])
 
     if st.button("Save"):
-        st.write(f"Email: {email}")
-        st.write(f"Date: {date}")
-        st.write(f"Time: {time}")
-        st.write(f"Sales Value: $ {value}")
-        st.write(f"Number of Products Sold: {n_produts}")
-        st.write(f"Category: {catogory}")
+        try:
+            date_time = datetime.combine(s_date, s_time)
+
+            sale = Sales(
+                    email = email,
+                    date = date_time,
+                    value = value,
+                    n_products = n_products,
+                    category = category
+                )
+            
+            st.write(sale)
+
+        except ValidationError as e:
+            st.error(f"Error {e}")
 
         
 
